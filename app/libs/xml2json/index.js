e2c68ee2c50972a49b15f0e3ee0dc6defb4e7737
Sergei Dolgov <Sergei.Dolgov@activeeon.com>
2014-05-16 18:44:05 +0200

    Commit app/libs as per https://www.npmjs.org/doc/faq.html

---
+++ app/libs/xml2json/index.js
@@ -0,0 +1,75 @@
+/*	This work is licensed under Creative Commons GNU LGPL License.
+
+	License: http://creativecommons.org/licenses/LGPL/2.1/
+   Version: 0.9
+	Author:  Stefan Goessner/2006
+	Web:     http://goessner.net/ 
+*/
+
+define(function (require) {
+
+    return {
+        parseXml: function(xml) {
+        var dom = null;
+        if (window.DOMParser) {
+            try {
+                dom = (new DOMParser()).parseFromString(xml, "text/xml");
+            }
+            catch (e) { dom = null; }
+        }
+        else if (window.ActiveXObject) {
+            try {
+                dom = new ActiveXObject('Microsoft.XMLDOM');
+                dom.async = false;
+                if (!dom.loadXML(xml)) // parse error ..
+
+                    console.log(dom.parseError.reason + dom.parseError.srcText);
+            }
+            catch (e) { dom = null; }
+        }
+        else
+            console.log("cannot parse xml string!");
+        return dom;
+    },
+
+// Changes XML to JSON
+   xmlToJson: function(xml) {
+        // Create the return object
+        var obj = {};
+        if (xml.nodeType == 1) { // element
+            // do attributes
+            if (xml.attributes.length > 0) {
+                obj["@attributes"] = {};
+                for (var j = 0; j < xml.attributes.length; j++) {
+                    var attribute = xml.attributes.item(j);
+                    obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+                }
+            }
+        } else if (xml.nodeType == 4) { // CDATA
+            var res = xml.nodeValue.replace("<![CDATA[", "");
+            obj = res.replace("]]>", "");
+        } else if (xml.nodeType == 3) { // text
+            obj = xml.nodeValue;
+        }
+
+        // do children
+        if (xml.hasChildNodes()) {
+            for(var i = 0; i < xml.childNodes.length; i++) {
+                var item = xml.childNodes.item(i);
+                var nodeName = item.nodeName;
+                if (typeof(obj[nodeName]) == "undefined") {
+                    obj[nodeName] = this.xmlToJson(item);
+                } else {
+                    if (typeof(obj[nodeName].push) == "undefined") {
+                        var old = obj[nodeName];
+                        obj[nodeName] = [];
+                        obj[nodeName].push(old);
+                    }
+                    obj[nodeName].push(this.xmlToJson(item));
+                }
+            }
+        }
+        return obj;
+    }
+    }
+})
