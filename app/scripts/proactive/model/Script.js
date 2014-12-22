56def30ab174d27c1a017992c3abe91b0b065017
Sergei Dolgov <Sergei.Dolgov@activeeon.com>
2014-12-22 18:08:12 +0100

    PWS-158 Remove the script library feature
    This feature proved impossible to use by regular users, and very
    confusing. The implementation is also problematic (DOM manipulation
    instead of proper Backbone approach).
    
    If we ever want to reimplement it, we'll need to rethink the interface.

--- app/scripts/proactive/model/Script.js
+++ app/scripts/proactive/model/Script.js
@@ -3,26 +3,20 @@ define(
         'backbone',
         'proactive/model/SchemaModel',
         'text!proactive/templates/script-form-template.html',
-        'proactive/rest/studio-client'
     ],
 
-    function (Backbone, SchemaModel, tpl, StudioClient) {
+    function (Backbone, SchemaModel, tpl) {
 
     "use strict";
 
     var scriptTemplate = _.template(tpl);
-    var serverScripts = function (callback) {
-        callback(["--inline--"].concat(StudioClient.listScripts()));
-    }
 
     return SchemaModel.extend({
         schema: {
-            "Library": {type: "Select", options: serverScripts},
-            "Library Path": {type: "Hidden"},
             "Script": {type: "TextArea", fieldAttrs: {'placeholder': ['code->#cdata-section', 'code->#text']}, template: scriptTemplate},
             "Engine": {type: 'Select', options: ["javascript", "groovy", "ruby", "python", "bash", "cmd", "R"], fieldAttrs: {'placeholder': 'code->@attributes->language', "data-help":"The runtime of the script."}},
             "Or Path": {type: "Hidden", fieldAttrs: {'placeholder': 'file->@attributes->path'}},
-            "Arguments": {type: 'List', itemType: 'Text', fieldAttrs: {'placeholder': 'file->arguments->argument', 'itemplaceholder': '@attributes->value'}},
+            "Arguments": {type: 'Hidden', itemType: 'Text', fieldAttrs: {'placeholder': 'file->arguments->argument', 'itemplaceholder': '@attributes->value'}},
             "Or Url": {type: "Hidden", fieldAttrs: {'placeholder': 'file->@attributes->url'}}
         },
 
--- app/scripts/proactive/model/SelectionScript.js
+++ app/scripts/proactive/model/SelectionScript.js
@@ -3,27 +3,21 @@ define(
         'backbone',
         'proactive/model/SchemaModel',
         'text!proactive/templates/script-form-template.html',
-        'proactive/rest/studio-client'
     ],
 
-    function (Backbone, SchemaModel, tpl, StudioClient) {
+    function (Backbone, SchemaModel, tpl) {
 
     "use strict";
 
     var scriptTemplate = _.template(tpl);
-    var serverScripts = function (callback) {
-        callback([""].concat(StudioClient.listScripts()));
-    }
 
     return SchemaModel.extend({
         // TODO inherit from Script - first attempt did not work because schema is shared - type appears in pre/post scripts as well
         schema: {
-            "Library": {type: "Select", options: serverScripts},
-            "Library Path": {type: "Hidden"},
             "Script": {type: "TextArea", fieldAttrs: {'placeholder': ['code->#cdata-section', 'code->#text']}, template: scriptTemplate},
             "Engine": {type: 'Select', options: ["javascript", "groovy", "ruby", "python", "bash", "cmd"], fieldAttrs: {'placeholder': 'code->@attributes->language'}},
             "Or Path": {type: "Hidden", fieldAttrs: {'placeholder': 'file->@attributes->path'}},
-            "Arguments": {type: 'List', itemType: 'Text', fieldAttrs: {'placeholder': 'file->arguments->argument', 'itemplaceholder': '@attributes->value'}},
+            "Arguments": {type: 'Hidden', itemType: 'Text', fieldAttrs: {'placeholder': 'file->arguments->argument', 'itemplaceholder': '@attributes->value'}},
             "Or Url": {type: "Hidden", fieldAttrs: {'placeholder': 'file->@attributes->url'}},
             "Type": {type: 'Select', options: ["dynamic", "static"], fieldAttrs: {'placeholder': '@attributes->type'}}
         },
