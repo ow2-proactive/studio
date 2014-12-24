781b195e6105534cd9848e877c7fe8f91f3d49be
youribonnaffe <youri.bonnaffe@activeeon.com>
2014-12-24 15:24:46 +0100

    More checking in breadcrumb test

--- tests/specs/breadcrumb.js
+++ tests/specs/breadcrumb.js
@@ -10,13 +10,17 @@ module.exports = {
             .click(".task")
             .assert.value('input[name="Task Name"]', 'Javascript_Task')
             .assert.containsText("#breadcrumb", 'Javascript_Task')
-
+            .assert.containsText("#breadcrumb", 'Untitled workflow')
+            .assert.containsText("#breadcrumb", 'Workflows')
 
             .click("#breadcrumb-selected-job")
-            .assert.value('input[name="Job Name"]', 'Untitled Job')
+            .assert.value('input[name="Job Name"]', 'Untitled workflow')
+            .assert.containsText("#breadcrumb", 'Untitled workflow')
+            .assert.containsText("#breadcrumb", 'Workflows')
 
             .click("#breadcrumb-list-workflows")
             .assert.containsText("#workflow-list", 'Project: Default')
+            .assert.containsText("#breadcrumb", 'Workflows')
 
             .end();
     },
