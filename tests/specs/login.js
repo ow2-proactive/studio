1a17840722dfa2a43e3bb6eca89e20924c349be1
youribonnaffe <youri.bonnaffe@activeeon.com>
2014-12-24 11:53:39 +0100

    Test for PWS-159

--- tests/specs/login.js
+++ tests/specs/login.js
@@ -15,6 +15,23 @@ module.exports = {
             .waitForElementVisible('.ui-pnotify-title', 1000)
             .assert.containsText('.ui-pnotify-title', 'Cannot connect to ProActive Studio')
             .end();
-    }
+    },
+
+    // (test fails only with Chrome)
+    "PWS-159 The dialog Please open a workflow appears behind the no workflow open panel": function (browser) {
+        browser
+            .login()
+
+            .click("#save-button")
+
+            .waitForElementVisible('#select-workflow-modal', 1000)
+            .assert.containsText('#select-workflow-modal h3', 'Please open a workflow')
+
+            .click("#select-workflow-modal .modal-footer .btn") // close button
+
+            .waitForElementNotVisible('#select-workflow-modal', 1000)
+
+            .end();
+    },
 
 };
