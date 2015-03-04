0195d2f09db06b12cc788e39c3b32c42b2f2e480
youribonnaffe <youri.bonnaffe@activeeon.com>
2015-03-04 11:00:30 +0100

    Update UI test after default workflow name change

--- test/ui/specs/breadcrumb.js
+++ test/ui/specs/breadcrumb.js
@@ -10,12 +10,12 @@ module.exports = {
             .click(".task")
             .assert.value('input[name="Task Name"]', 'Javascript_Task')
             .assert.containsText("#breadcrumb", 'Javascript_Task')
-            .assert.containsText("#breadcrumb", 'Untitled workflow')
+            .assert.containsText("#breadcrumb", 'Untitled Job')
             .assert.containsText("#breadcrumb", 'Workflows')
 
             .click("#breadcrumb-selected-job")
-            .assert.value('input[name="Job Name"]', 'Untitled workflow')
-            .assert.containsText("#breadcrumb", 'Untitled workflow')
+            .assert.value('input[name="Job Name"]', 'Untitled Job')
+            .assert.containsText("#breadcrumb", 'Untitled Job')
             .assert.containsText("#breadcrumb", 'Workflows')
 
             .click("#breadcrumb-list-workflows")
