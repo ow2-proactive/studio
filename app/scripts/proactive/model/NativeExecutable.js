2d2d7c9334f8e53aaaf8e027cf33e4a18908e1c7
Laurent Pellegrino <laurent.pellegrino@activeeon.com>
2015-07-03 10:02:57 +0200

    Remove dynamic command from user interface

--- app/scripts/proactive/model/NativeExecutable.js
+++ app/scripts/proactive/model/NativeExecutable.js
@@ -14,8 +14,6 @@ define(
             "Static Command": {type: "Text", fieldAttrs: {'placeholder': 'staticCommand->@attributes->value', "data-help":'A native executable to run. Must not contain parameters. E.g. for command \"/bin/bash echo test\" only /bin/bash should be specified here. Everything else should be set as arguments.'}},
             "Working Folder": {type: "Text", fieldAttrs: {'placeholder': 'staticCommand->@attributes->workingDir', "data-help":'A working folder of the executable on computing nodes.'}},
             "Arguments": {type: 'List', itemType: 'Text', fieldAttrs: {'placeholder': 'staticCommand->arguments->argument', 'itemplaceholder': '@attributes->value', "data-help":'Command line arguments. Do not use space as a separator for arguments. Rather add all of arguments one by one to the list.'}},
-            "Or Dynamic Command": {type: 'NestedModel', model: Script, fieldAttrs: {'placeholder': 'dynamicCommand->generation->script', "data-help":'Alternatively you can specify a script that generates command line.'}},
-            "Working Dir": {type: "Text", fieldAttrs: {'placeholder': 'dynamicCommand->@attributes->workingDir', "data-help":'A working folder of the script on computing nodes.'}}
         }
     })
 })
