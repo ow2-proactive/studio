c632a5aa9807bd7a5b84e4801e5670f9105b29e2
Laurent Pellegrino <laurent.pellegrino@activeeon.com>
2015-10-02 15:28:35 +0200

    Sort examples by alphabetical order

--- app/scripts/proactive/config.js
+++ app/scripts/proactive/config.js
@@ -2,21 +2,21 @@ define(function (require) {
     return {
         restApiUrl: '/rest/studio',
         tasks: {
-            "Native": 'templates/04_native_task.xml',
-            "Java": 'templates/java.xml',
-            "Javascript": 'templates/script_javascript.xml',
-            "Groovy": 'templates/script_groovy.xml',
-            "Ruby": 'templates/script_ruby.xml',
-            "Python": 'templates/script_python.xml',
             "Bash": 'templates/script_bash.xml',
             "Cmd": 'templates/script_cmd.xml',
-            "Language R": 'templates/script_r.xml'
+            "Groovy": 'templates/script_groovy.xml',
+            "Java": 'templates/java.xml',
+            "Javascript": 'templates/script_javascript.xml',
+            "Native": 'templates/04_native_task.xml',
+            "Python": 'templates/script_python.xml',
+            "Ruby": 'templates/script_ruby.xml',
+            "R": 'templates/script_r.xml'
         },
         controls: {
-            "Task Dependencies": 'templates/02_task_dependencies.xml',
             "If": 'templates/07_workflow_branch.xml',
             "Loop": 'templates/06_workflow_loop.xml',
-            "Replicate": 'templates/05_workflow_replication.xml'
+            "Replicate": 'templates/05_workflow_replication.xml',
+            "Task Dependencies": 'templates/02_task_dependencies.xml'
         }
     }
 })
