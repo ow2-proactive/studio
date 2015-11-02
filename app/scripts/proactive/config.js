01fb0c82dd167ffdc039c8ff086181926af09d92
Laurent Pellegrino <laurent.pellegrino@activeeon.com>
2015-11-02 11:31:28 +0100

    Reorder tasks and update names based on Denis' wishes

--- app/scripts/proactive/config.js
+++ app/scripts/proactive/config.js
@@ -2,15 +2,14 @@ define(function (require) {
     return {
         restApiUrl: '/rest/studio',
         tasks: {
-            "Bash": 'templates/script_bash.xml',
-            "Cmd": 'templates/script_cmd.xml',
-            "Groovy": 'templates/script_groovy.xml',
+            "Linux Bash": 'templates/script_bash.xml',
+            "Windows Cmd": 'templates/script_cmd.xml',
             "Java": 'templates/java.xml',
             "Javascript": 'templates/script_javascript.xml',
-            "Native": 'templates/04_native_task.xml',
-            "Python": 'templates/script_python.xml',
+            "Groovy": 'templates/script_groovy.xml',
             "Ruby": 'templates/script_ruby.xml',
-            "R": 'templates/script_r.xml'
+            "Python": 'templates/script_python.xml',
+            "Language R": 'templates/script_r.xml'
         },
         controls: {
             "If": 'templates/07_workflow_branch.xml',
