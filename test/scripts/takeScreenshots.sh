fbdf43e136323b75e849308dd51b14d661ae04e0
Tobias Wiens <tobias.wiens@activeeon.com>
2016-02-02 12:33:27 +0000

    Fix relative path in screenshot script.

--- test/scripts/takeScreenshots.sh
+++ test/scripts/takeScreenshots.sh
@@ -4,7 +4,7 @@
 # Use export DISPLAY=:99 to capture screenshots from display 99.
 for i in {1..100000}
 do
-    xwd -root -silent | xwdtopnm |pnmtojpeg > "../ui/reports/screen$i.jpg"
+    xwd -root -silent | xwdtopnm |pnmtojpeg > "test/ui/reports/screen$i.jpg"
    echo "Took screenshot number: $i"
    sleep 0.5s
 done
