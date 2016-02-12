4303853a28924d1ca9b9ebcaec177e2e3947f69b
Tobias Wiens <tobias.wiens@activeeon.com>
2016-02-12 16:14:36 +0100

    Prevent the Studio test run into an infinite loop of failures.
    The selenium tests have some parts, which retry in an infinite loop. That can and did let the test run into an infinite loop, blocking jenkins and flooding the file system with error reports. To prevent that from happening again, a maximum number of tries is introduced.

--- test/ui/commands/hideMenu.js
+++ test/ui/commands/hideMenu.js
@@ -5,16 +5,17 @@
 exports.command = function () {
     var browser = this
 
-    var hideMenu = function () {
+    var hideMenu = function (iteration, amount) {
         // isVisible will cause a test failure if element cannot be found, fallback to Selenium method
         browser.element("css selector", "div.navbar-collapse.collapsed", function (result) {
-            if (result.status === 0) {
+            console.log("Hide menu, iteration: "+iteration+" max repetitions: "+amount)
+            if (result.status === 0 && iteration < amount) {
                 browser.click('.navbar-toggle')
-                hideMenu()
+                hideMenu(iteration++, amount)
             }
         })
     }
-    hideMenu();
+    hideMenu(0, browser.globals.numberOfTries);
 
     return this;
 };
