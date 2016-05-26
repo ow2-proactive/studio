9508d745099f788b551e6fcc409f51d3ee3b2c34
sophiesongge <sophiesongge@gmail.com>
2016-05-26 21:30:12 +0200

    add catalog template

---
+++ app/scripts/proactive/view/CatalogView.js
@@ -0,0 +1,26 @@
+define(
+    [
+        'jquery',
+        'backbone',
+        'text!proactive/templates/catalog-browser.html',
+    ],
+
+    function ($, Backbone, catalogBrowser) {
+
+    "use strict";
+
+    return Backbone.View.extend({
+        template: _.template(catalogBrowser),
+        initialize: function () {
+            var that = this;
+            this.$el = $("<div id='catalog-browser-container'></div>");
+            $("#catalog-browser-body").append(this.$el);
+            this.render();
+        },
+        render: function () {
+            this.$el.html(this.template())
+            return this;
+        }
+    })
+
+})
--- app/scripts/proactive/view/dom.js
+++ app/scripts/proactive/view/dom.js
@@ -109,6 +109,13 @@ define(
             $('#xml-view-modal').modal();
         })
 
+        $("#browse-catalog-button").click(function (event) {
+            event.preventDefault();
+            var StudioApp = require('StudioApp');
+            StudioApp.views.catalogView.render();
+            $('#catalog-browser-view-modal').modal();
+        })
+
         $("#layout-button").click(function (event) {
             event.preventDefault();
             require('StudioApp').views.workflowView.autoLayout();
