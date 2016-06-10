ddeab371e33a8c2a966028dfa24b3ee3a2bb259a
Paraita Wohler <paraita.wohler@activeeon.com>
2016-06-10 08:05:33 +0200

    Replace the checkbox by bootstrap images and change their state dynamically on click

--- app/scripts/proactive/view/CatalogWorkflowItem.js
+++ app/scripts/proactive/view/CatalogWorkflowItem.js
@@ -16,6 +16,7 @@ define(
             events: {
                 "click #btn-pull-workflow-xml": "pullWorkflow",
                 "click #btn-remove-workflow": "removeConfirm",
+		"click #btn-toggle-removal-state": "toggleRemovalState"
             },
             // Pull from the Catalog to the Studio
             pullWorkflow: function(e){
@@ -38,13 +39,48 @@ define(
                 StudioApp.workflowToRemove = this.model;
                 $('#delete-workflow-confirmation-modal').modal();
 
+                // get the current el into a local var
+                var elToModify = this.$el;
+
+                console.log('elToModify');
+                console.log(elToModify);
+
+                // replace in the local var
+                console.log('toggling removal state !');
+                if ($('#btn-toggle-removal-state input', elToModify).prop('checked')) {
+                    $('#btn-toggle-removal-state input', elToModify).prop('checked', false);
+                    console.log('element to uncheck:');
+                    console.log($('#btn-toggle-removal-state i', elToModify));
+                    $('#btn-toggle-removal-state i', elToModify)
+                        .removeClass()
+                        .addClass('state-icon glyphicon glyphicon-unchecked');
+                    console.log('element after modification:');
+                    console.log($('#btn-toggle-removal-state i', elToModify));
+
+                }
+                else {
+                    $('#btn-toggle-removal-state input', elToModify).prop('checked', true);
+                    console.log('element to check:');
+                    console.log($('#btn-toggle-removal-state i', elToModify));
+                    $('#btn-toggle-removal-state i', elToModify)
+                        .removeClass()
+                        .addClass('state-icon glyphicon glyphicon-check');
+                    console.log('element after modification:');
+                    console.log($('#btn-toggle-removal-state i', elToModify));
+                }
+
+                // replace the dom el by this local var, replace this.el with local var
+                console.log('new state:');
+                console.log($('#btn-toggle-removal-state input', elToModify).prop('checked'));
+                this.$el.replaceWith(elToModify);
+                this.setElement(elToModify);
             },
             removeWorkflow: function(event) {
 
             },
             render: function () {
                 var workflowList = _.template(catalogWorkflowList);
-                $(this.el).append(workflowList({ workflow: this.model }));
+                $(this.el).html(workflowList({ workflow: this.model }));
                 return this;
             }
         })
