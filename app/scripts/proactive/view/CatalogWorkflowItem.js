ae4d7d4d0627bd893c75faaeb2e5612d9edb3c35
Paraita Wohler <paraita.wohler@activeeon.com>
2016-06-10 09:30:25 +0200

    Show the number of ticked workflows in the delete button from the Catalog browser view

--- app/scripts/proactive/view/CatalogWorkflowItem.js
+++ app/scripts/proactive/view/CatalogWorkflowItem.js
@@ -15,7 +15,6 @@ define(
             tagName: "li",
             events: {
                 "click #btn-pull-workflow-xml": "pullWorkflow",
-                "click #btn-remove-workflow": "removeConfirm",
 		        "click #btn-toggle-removal-state": "toggleRemovalState"
             },
             // Pull from the Catalog to the Studio
@@ -47,11 +46,34 @@ define(
                         .removeClass()
                         .addClass('state-icon glyphicon glyphicon-check');
                 }
+                this._notifyDeleteButton();
             },
             removeConfirm: function(){
                 var StudioApp = require('StudioApp');
                 StudioApp.workflowToRemove = this.model;
                 $('#delete-workflow-confirmation-modal').modal();
+            },
+            _notifyDeleteButton: function() {
+                var deleteButton = $('#delete-selection-catalog');
+                console.log('Delete button:');
+                console.log(deleteButton);
+
+                // how many workflowitems are ticked ?
+                var nbTickedWorkflows = $('#catalog-workflow-list li').has('input:checkbox:checked').length;
+                console.log('Ticked workflows:');
+                console.log(nbTickedWorkflows);
+
+                // if there's at least one ticked workflow item, enable the button
+                if (nbTickedWorkflows > 0) {
+                    deleteButton.text("Delete (" + nbTickedWorkflows + ")");
+                    deleteButton.prop('disabled', false);
+                }
+                else {
+                    deleteButton.text("Delete");
+                    deleteButton.prop('disabled', true);
+                }
+
+
             },
             render: function () {
                 var workflowList = _.template(catalogWorkflowList);
