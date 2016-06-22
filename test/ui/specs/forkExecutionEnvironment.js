5c1a604f622de13df2cf49559e7043f48fc8b0e7
Paraita Wohler <paraita.wohler@activeeon.com>
2016-06-22 15:24:22 +0200

    Stabilize nightwatch test

--- test/ui/specs/forkExecutionEnvironment.js
+++ test/ui/specs/forkExecutionEnvironment.js
@@ -12,7 +12,9 @@ module.exports = {
             .useXpath() // every selector now must be xpath
             .waitForElementVisible('//*[@id=\"Fork Environment\"]') // Check for the Fork Environment tab
             .moveToElement('//*[@id=\"Fork Environment\"]', 10, 10)
+            .pause(browser.globals.waitForConditionTimeout)
             .click('//*[@id=\"Fork Environment\"]') // Click on the Fork Environment tab
+            .pause(browser.globals.waitForConditionTimeout)
             // Wait for the For Execution Environment select to be visible
             .waitForElementVisible('//select[@name=\"Fork Execution Environment\"]')
             .useCss() // we're back to CSS now
@@ -31,6 +33,7 @@ module.exports = {
             .useXpath() // every selector now must be xpath
             .waitForElementVisible('//*[@id=\"Fork Environment\"]') // Check for the Fork Environment tab
             .moveToElement('//*[@id=\"Fork Environment\"]', 10, 10)
+            .pause(browser.globals.waitForConditionTimeout)
             .click('//*[@id=\"Fork Environment\"]') // Click on the Fork Environment tab
             .pause(browser.globals.waitForConditionTimeout)
             // Wait for the For Execution Environment select to be visible
