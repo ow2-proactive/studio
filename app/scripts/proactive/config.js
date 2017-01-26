fa4f43cd20352ad9cb556182271d86714aa9c464
Shatalov <yaroslav.shatalov@activeeon.com>
2017-01-26 16:52:00 +0100

    add LDAP Query Task

--- app/scripts/proactive/config.js
+++ app/scripts/proactive/config.js
@@ -16,7 +16,8 @@ define(function () {
             'Ruby': 'templates/script_ruby.xml',
             'Python': 'templates/script_python.xml',
             'Language R': 'templates/script_r.xml',
-            'Cron': 'templates/script_cron.xml'
+            'Cron': 'templates/script_cron.xml',
+            'LDAP Query': 'templates/script_ldap_query.xml'
         },
         manuals: {
             'Email Notification' : 'templates/email.xml',
