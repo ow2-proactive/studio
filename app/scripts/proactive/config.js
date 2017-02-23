fbcb1be07cccc3653a3fb8943071a85fecb2ee4f
MaelAudren <mael.audren@activeeon.com>
2017-02-23 16:41:54 +0100

    add perl script engine

--- app/scripts/proactive/config.js
+++ app/scripts/proactive/config.js
@@ -24,6 +24,7 @@ define(function () {
             'Ruby': 'templates/script_ruby.xml',
             'Python': 'templates/script_python.xml',
             'Language R': 'templates/script_r.xml',
+	    'Perl': 'templates/script_perl.xml',
             'Cron': 'templates/script_cron.xml',
             'LDAP Query': 'templates/script_ldap_query.xml'
         },
