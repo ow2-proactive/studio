68a635d2a29021276c5de4d99360ae110ffb3f08
chloegugli <chloe.guglielmi@activeeon.com>
2017-10-10 15:56:10 +0200

    Message displayed while waited for buckets to update

--- app/scripts/proactive/view/dom.js
+++ app/scripts/proactive/view/dom.js
@@ -130,7 +130,9 @@ define(
         $("#get-from-catalog-button").click(function (event) {
             event.preventDefault();
             var studioApp = require('StudioApp');
+            $('#catalog-loading-modal').modal();
             studioApp.models.catalogBuckets.fetch({reset: true, async: false});
+            $('#catalog-loading-modal').modal('hide');
             studioApp.modelsToRemove = [];
             studioApp.views.catalogGetView.render();
             $('#catalog-get-modal').modal();
@@ -140,7 +142,9 @@ define(
             event.preventDefault();
             var studioApp = require('StudioApp');
             if (studioApp.isWorkflowOpen()){
+                $('#catalog-loading-modal').modal();
                 studioApp.models.catalogBuckets.fetch({reset: true, async: false});
+                $('#catalog-loading-modal').modal('hide');
                 studioApp.modelsToRemove = [];
                 studioApp.views.catalogPublishView.render();
                 $('#catalog-publish-modal').modal();
