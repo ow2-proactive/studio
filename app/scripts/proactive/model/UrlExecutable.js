e52a4c7ff655d9a574b537c80f6bd24b137870b2
sophiesongge <sophiesongge@gmail.com>
2017-12-01 15:00:05 +0100

    add 'languange' options for executing from a URL

--- app/scripts/proactive/model/UrlExecutable.js
+++ app/scripts/proactive/model/UrlExecutable.js
@@ -16,6 +16,14 @@ define(
                     'placeholder': 'file->@attributes->url',
                     "data-help":'A Url which contains the Script to be executed.'
                 }
+            },
+            "Language": {
+                type: 'Select',
+                options: [" ", "bash", "cmd", "docker-compose", "groovy", "javascript", "python", "cpython", "ruby", "perl", "powershell", "R"],
+                fieldAttrs: {
+                    'placeholder': 'code->@attributes->language',
+                    "data-help":"The language of the code to execute."
+                }
             }
         }
     })
