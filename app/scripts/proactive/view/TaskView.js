a02c8c0913ab98982db81cf6b6e8ae5436b5256d
Tobias Wiens <tobwiens@gmail.com>
2017-12-22 11:39:05 +0100

    Add PHP task
    - Add PHP task to 'Tasks' menu.
    - Include icons

--- app/scripts/proactive/view/TaskView.js
+++ app/scripts/proactive/view/TaskView.js
@@ -19,7 +19,7 @@ define(
     	
         icons: {"JavaExecutable": "images/Java.png", "NativeExecutable": "images/command.png", "ScriptExecutable": "images/script.png"},
         iconsPerLanguage: {"java": "images/Java.png", "groovy": "images/Groovy.png", "docker-compose": "images/Docker.png",
-        	"bash": "images/LinuxBash.png", "javascript": "images/Javascript.png", "cmd": "images/WindowsCmd.png", "ruby": "images/Ruby.png", 
+        	"bash": "images/LinuxBash.png", "javascript": "images/Javascript.png", "PHP" : "images/PHP.png", "cmd": "images/WindowsCmd.png", "ruby": "images/Ruby.png",
         		   "R": "images/R.png", "python": "images/Jython.png", "cpython": "images/Python.png", "cron": "images/Cron.png", "LDAP Query": "images/LDAPQuery.png", "perl": "images/Perl.png", "powershell": "images/PowerShell.png"},
         controlFlows: {"dependency": true, "if": false, "replicate": false, "loop": false},
 
