f7d4f82258ebcf66dfcfcba47d9e51e7095c784e
Aminelouati <louati.amiine@gmail.com>
2018-05-10 12:48:29 +0200

    fix project name display when drag\&drop a workflow from the palette

--- app/scripts/proactive/model/SchemaModel.js
+++ app/scripts/proactive/model/SchemaModel.js
@@ -184,7 +184,7 @@ define(
                                     console.log("Should no be here", prop, value);
                                 }
                             } else {
-                                if (merging && that.get(prop) && that.get(prop).includes('Untitled Workflow')){
+                                if (merging && that.get(prop) && (that.get(prop).includes('Untitled Workflow') || that.get(prop).includes('Untitled workflow'))){
                                     value = value.trim()
                                     that.set(prop, value)
                                 }
