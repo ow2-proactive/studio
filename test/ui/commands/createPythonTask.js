6ade558571e28df392f2eb4868aaeaf9c572c16b
fviale <fabien.viale@activeeon.com>
2018-05-25 17:39:32 +0200

    New CodeMirror Editor  - trying to fix test which does not pick the right task

--- test/ui/commands/createPythonTask.js
+++ test/ui/commands/createPythonTask.js
@@ -3,8 +3,8 @@ exports.command = function () {
     this
         .waitForElementVisible('#task-menu')
         .click('#task-menu')
-        .waitForElementVisible('#Python')
-        .moveToElement('#Python', 8, 8)
+        .waitForElementVisible('#Jython')
+        .moveToElement('#Jython', 8, 8)
         .mouseButtonDown(0)
         .moveToElement('#workflow-designer', 100, 100)
         .mouseButtonUp(0);
