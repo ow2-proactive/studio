79b4f441d725b4c10be0d0de337a64adc6293bb3
Pedro Velho <pedro.velho@activeeon.com>
2018-05-31 16:10:46 +0200

    Added javascript logic to show hide elements as expected.

--- app/scripts/proactive/view/LoginView.js
+++ app/scripts/proactive/view/LoginView.js
@@ -70,6 +70,50 @@ define(
 
                 $('body').append(that.$el).show();
 
+                $('#login-option-plus').click(function () {
+                    $('#login-option-plus').hide();
+                    $('#login-option-minus').show();
+                    $('#login-options').show();
+                    console.log("login-mode value : "+$('#login-mode').val());
+                    if($('#login-mode').val() === "basic"){
+                        $('#login-credentials').hide();
+                        $('#login-ssh').show();
+                    }else{
+                        $('#login-credentials').show();
+                        $('#login-ssh').hide();
+                        $('#login-basic').hide();
+                    }
+                });
+
+                $('#login-option-minus').click(function () {
+                    $('#login-option-minus').hide();
+                    $('#login-option-plus').show();
+                    $('#login-options').hide();
+                    $('#login-credentials').hide();
+                    $('#login-ssh').hide();
+                    $('#login-basic').show();
+                })
+
+                $('#login-mode').change(function () {
+                   if($('#login-mode').val() === "basic"){
+                       $('#login-credentials').hide();
+                       $('#login-basic').show();
+                       $('#login-ssh').show();
+                   }else{
+                       $('#login-credentials').show();
+                       $('#login-ssh').hide();
+                       $('#login-basic').hide();
+                   }
+                });
+
+                $('#login-ssh-checkbox').change(function () {
+                    if($(this).is(":checked")) {
+                        $('#login-ssh-button').show();
+                    }else{
+                        $('#login-ssh-button').hide();
+                    }
+                });
+
                 StudioClient.isConnected(function() {
                     // logged in successfully - show user name
                     console.log("Logged in");
