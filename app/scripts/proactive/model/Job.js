5255d488afb460d437e57264a0a5c43653e12567
tao-xinxiu <isabelle.tao@activeeon.com>
2019-08-09 11:38:10 +0200

    feat [taskRetryDelay]: add the taskRetryDelay property in job and task error management.

--- app/scripts/proactive/model/Job.js
+++ app/scripts/proactive/model/Job.js
@@ -218,6 +218,13 @@ define(
             "data-help": "Defines whether tasks that have to be restarted will restart on an other computing node."
           },
           options: ["anywhere", "elsewhere"]
+        },
+        "Delay Before Retry Task (hh:mm:ss)": {
+            type: "Text",
+            fieldAttrs: {
+                'placeholder': '@attributes->taskRetryDelay',
+                "data-help": 'Specifies how long to wait before restart the task in error. <br/><br/>Format is the following:<br/><br/>5 means 5 seconds<br/><br/>10:5 means 10 minutes 5 seconds<br/><br/>1:02:03 is 1 hour 2 minutes and 3 seconds.'
+            }
         }
       },
       initialize: function() {
