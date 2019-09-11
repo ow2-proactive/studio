5bf4ec37bdbf4020c7c301d3b982026d8f4a08b4
tao-xinxiu <isabelle.tao@activeeon.com>
2019-09-11 15:59:14 +0200

    fix [PA:CREDENTIAL]: move third-party-credential modal into separte html, fix the pb of creating duplicated gray layer when add/remove 3rd party credential.

--- app/scripts/proactive/view/JobVariableView.js
+++ app/scripts/proactive/view/JobVariableView.js
@@ -1,9 +1,10 @@
 define(
     [
         'backbone',
-        'text!proactive/templates/job-variable-template.html'
+        'text!proactive/templates/job-variable-template.html',
+        'proactive/view/ThirdPartyCredentialView'
     ],
-    function (Backbone, jobVariableTemplate) {
+    function (Backbone, jobVariableTemplate, ThirdPartyCredentialView) {
 
     "use strict";
 
@@ -15,9 +16,6 @@ define(
 
         events: {
             'click #third-party-credential-button': 'showThirdPartyCredentialModal',
-            'click .third-party-credential-close': 'closeThirdPartyCredential',
-            'click #add-third-party-credential-button': 'addThirdPartyCredential',
-            'click .remove-third-party-credential': 'removeThirdPartyCredential'
         },
 
         initialize: function () {
@@ -32,6 +30,10 @@ define(
         },
 
         showThirdPartyCredentialModal: function() {
+            new ThirdPartyCredentialView().render();
+        },
+
+        refreshThirdPartyCredential: function() {
             var that = this;
             $.ajax({
                 url: "/rest/scheduler/credentials/",
@@ -42,35 +44,6 @@ define(
                     that.$el.html(that.template(that.viewInfos));
                 }
             });
-            $('#third-party-credential-modal').modal();
-        },
-
-        addThirdPartyCredential: function(event) {
-            this.thirdPartyCredentialRequest($('#new-cred-key').val(), "POST", { value: $('#new-cred-value').val() });
-        },
-
-        removeThirdPartyCredential: function(event) {
-            this.thirdPartyCredentialRequest(event.target.id, "DELETE", {});
-        },
-
-        thirdPartyCredentialRequest: function(credentialKey, typeRequest, requestData) {
-            var that = this;
-            $.ajax({
-                url: "/rest/scheduler/credentials/" + credentialKey,
-                type: typeRequest,
-                data: requestData,
-                headers: { "sessionid": localStorage['pa.session'] },
-                success: function (data) {
-                    that.showThirdPartyCredentialModal();
-                },
-                error: function (xhr, status, error) {
-                    alert('Failed to edit the third-party credential.' + xhr.status + ': ' + xhr.statusText);
-                }
-            });
-        },
-
-        closeThirdPartyCredential: function (event) {
-            $('#third-party-credential-modal').modal('hide');
         }
     })
 })
