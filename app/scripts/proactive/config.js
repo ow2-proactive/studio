4e52e31cb1780ab42b2e6bdf8b54c54ddf3ee593
Fabien Viale <fabien.viale@activeeon.com>
2019-09-26 14:59:11 +0200

    Change docker fork environment script:  - use groovy  - store the script content in the studio config  - implement impersonation using sigar

--- app/scripts/proactive/config.js
+++ app/scripts/proactive/config.js
@@ -174,6 +174,41 @@ define(function () {
                 'name': 'Big Data',
                 'buckets': ['controls', 'notification-tools', 'basic-examples', 'big-data', 'data-connectors', 'database-services']
             }
-        ]
+        ],
+        docker_env_script: `
+// In the Java Home location field, use the value: "/usr" to force using the JRE provided in the docker image below (Recommended).
+// Be aware, that the prefix command is internally split by spaces. So paths with spaces won't work.
+// Prepare Docker parameters
+containerName = "java"
+dockerRunCommand =  "docker run "
+dockerParameters = "--rm "
+// Prepare ProActive home volume
+paHomeHost = variables.get("PA_SCHEDULER_HOME")
+paHomeContainer = variables.get("PA_SCHEDULER_HOME")
+proActiveHomeVolume = '-v '+paHomeHost +':'+paHomeContainer+' '
+// Prepare working directory (For Dataspaces and serialized task file)
+workspaceHost = localspace
+workspaceContainer = localspace
+workspaceVolume = '-v '+localspace +':'+localspace+' '
+// Prepare container working directory
+containerWorkingDirectory = '-w '+workspaceContainer+' '
+
+sigar = new org.hyperic.sigar.Sigar()
+try {
+    pid = sigar.getPid()
+    creds = sigar.getProcCred(pid)
+    uid = creds.getUid()
+    gid = creds.getGid()
+    userDefinition = "--user=" + uid + ":" + gid + " "
+} catch (Exception e) {
+    println "Cannot retrieve user or group id : " + e.getMessage()
+    userDefinition = "";
+} finally {
+    sigar.close()
+}
+
+// Save pre execution command into magic variable 'preJavaHomeCmd', which is picked up by the node
+preJavaHomeCmd = dockerRunCommand + dockerParameters + proActiveHomeVolume + workspaceVolume + userDefinition + containerWorkingDirectory + containerName
+        `
     };
 });
