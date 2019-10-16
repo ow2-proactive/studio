38020d36ea8bf4f869b7e8c8a7d9854ae7d218ca
tao-xinxiu <isabelle.tao@activeeon.com>
2019-10-16 09:55:05 +0200

    fix: zIndex modification should only apply to the 3rd-party-cred modal to avoid impact catalog script import

--- app/scripts/proactive/view/JobVariableView.js
+++ app/scripts/proactive/view/JobVariableView.js
@@ -21,7 +21,7 @@ define(
         initialize: function () {
             this.$el = $('#job-variables');
             // fix overlays of nested modal "third-party-credential-modal" inside "execute-workflow-modal" (backdrop overlays the previous modal)
-            $(document).on('show.bs.modal', '.modal', function () {
+            $(document).on('show.bs.modal', '.nested-modal', function() {
                 var zIndex = 1040 + (10 * $('.modal:visible').length);
                 $(this).css('z-index', zIndex);
                 // setTimeout is used because the .modal-backdrop isn't created when the event show.bs.modal is triggered.
