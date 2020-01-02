ae89aee0119d97f2d5e1e440d37d4c6dc7b20434
medou-boushab <medou.boushab@activeeon.com>
2020-01-02 11:54:28 +0100

    keep line breaks in description

--- app/scripts/proactive/view/TaskView.js
+++ app/scripts/proactive/view/TaskView.js
@@ -101,7 +101,7 @@ define(
             //Show the task description on a modal.
             $(".task-descritption").click(function (event) {
                 $('#task-description-modal').modal();
-                $("#task-description-container").text( $(".selected-task").find(".task-name").attr('title') );
+                $("#task-description-container").html($(".selected-task").find(".task-name").attr('title').replace(/</g, "&lt;").replace(/\n/g, "<br/>"));
             })
         },
 
