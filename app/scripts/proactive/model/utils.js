632a2217e53a02e7ffa0c1759eb13fa3cf2275f8
fviale <fabien.viale@activeeon.com>
2022-04-28 18:13:36 +0200

    Improve special characters handling
    Escape characters when appropriate

--- app/scripts/proactive/model/utils.js
+++ app/scripts/proactive/model/utils.js
@@ -9,17 +9,17 @@ define(
         return {
             inlineName: function(prop) {
                 var name = prop['Name'] ? prop['Name'] : prop['Property Name'];
-                return name;
+                return _.escape(name);
             },
             inlineNameValueNotEditable: function(prop) {
-                return "<div class='property-field'>" + prop['Name'] + "</div><div class='property-field'>" + prop["Value"] + "</div>";
+                return "<div class='property-field'>" + _.escape(prop['Name']) + "</div><div class='property-field'>" + _.escape(prop["Value"]) + "</div>";
             },
             inlineNameValue: function(prop) {
                 var name = prop['Name'] ? prop['Name'] : prop['Property Name'];
                 var value = prop['Value'] ? prop['Value'] : prop['Property Value'];
 
-                return "<input class='input-property-field' type=\"text\" value=\"" + name +
-                "\"><input class='input-property-field' type=\"text\"  value=\""+ value + "\">";
+                return "<input class='input-property-field' type=\"text\" value=\"" + _.escape(name) +
+                "\"><input class='input-property-field' type=\"text\"  value=\""+ _.escape(value) + "\">";
             },
             inlineNameValueInherited: function(prop) {
                 prop['Inherited'] = (prop['Inherited'] && (prop['Inherited'] == true || prop['Inherited'] == "true"));
@@ -30,8 +30,8 @@ define(
                     checked=" checked"
                 }
 
-                return "<input class='input-property-field' type=\"text\" value=\"" + prop['Name'] +
-                "\"><input class='input-property-field' type=\"text\"  value=\""+ value +
+                return "<input class='input-property-field' type=\"text\" value=\"" + _.escape(prop['Name']) +
+                "\"><input class='input-property-field' type=\"text\"  value=\""+ _.escape(value) +
                 "\"><input class='input-property-field' type=\"checkbox\"" + checked + " onclick=\"return false;\">";
             },
             bigCrossTemplate: _.template('<div><span data-editor></span><button type="button" class="btn btn-danger" data-action="remove">x</button></div>'),
