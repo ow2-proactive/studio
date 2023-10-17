bfa6da346f465d57f457b14d9b7d630023f14420
Alexandre Falessi <53172400+AlexFalessi@users.noreply.github.com>
2023-10-17 16:21:28 -0400

    Update variables dispplay: advanced orange, hidden grey, normal black (#932)
    * Update variables dispplay: advanced orange, hidden grey, normal black
    
    * Fix: Hidden takes precedence over advanced (not displayed if variable is advanced AND hidden). + Remove useless class apply

--- app/scripts/proactive/model/utils.js
+++ app/scripts/proactive/model/utils.js
@@ -17,8 +17,14 @@ define(
             inlineNameValue: function(prop) {
                 var name = prop['Name'] ? prop['Name'] : prop['Property Name'];
                 var value = prop['Value'] ? prop['Value'] : prop['Property Value'];
-
-                return "<input class='input-property-field' type=\"text\" value=\"" + _.escape(name) +
+                var color = "";
+                if (prop['Advanced'] == true) {
+                    color = "color:#E86D1F;";
+                }
+                if (prop['Hidden'] == true) {
+                    color = "color:#888;";
+                }
+                return "<input class='input-property-field' style='" + color + "' type=\"text\" value=\"" + _.escape(name) +
                 "\"><input class='input-property-field' type=\"text\"  value=\""+ _.escape(value) + "\">";
             },
             inlineNameValueInherited: function(prop) {
@@ -29,8 +35,14 @@ define(
                 if (prop['Inherited']){
                     checked=" checked"
                 }
-
-                return "<input class='input-property-field' type=\"text\" value=\"" + _.escape(prop['Name']) +
+                var color = "";
+                if (prop['Advanced'] == true) {
+                    color = "color:#E86D1F;";
+                }
+                if (prop['Hidden'] == true) {
+                    color = "color:#888;";
+                }
+                return "<input class='input-property-field' style='" + color + "' type=\"text\" value=\"" + _.escape(prop['Name']) +
                 "\"><input class='input-property-field' type=\"text\"  value=\""+ _.escape(value) +
                 "\"><input class='input-property-field' type=\"checkbox\"" + checked + " onclick=\"return false;\">";
             },
