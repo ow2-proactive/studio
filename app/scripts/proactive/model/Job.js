81a50720b916ed1f003af99e7b302f311d6df905
aminelouati <louati.amiine@gmail.com>
2024-10-01 16:06:14 +0200

    remove new line break

--- app/scripts/proactive/model/Job.js
+++ app/scripts/proactive/model/Job.js
@@ -208,7 +208,6 @@ define(
                 'listseparator': /\s*,\s*/,
                 'data-tab' : 'Workflow Tags',
                 "data-help": "Enable the end-user to attach any kind of information to workflows to organize, search, and manage them more effectively"
-
             },
             itemTemplate: Utils.bigCrossTemplate
         },
